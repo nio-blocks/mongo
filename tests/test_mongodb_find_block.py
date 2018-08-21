e39cdc6a7ec658483a8f1730eb71c1ee99610325
Tom Young <tyoung@n.io>
2018-08-21 17:02:56 -0600

    add failing test for enriching no_results

--- tests/test_mongodb_find_block.py
+++ tests/test_mongodb_find_block.py
@@ -35,3 +35,23 @@ class TestMongoDB(NIOBlockTestCase):
             self.last_notified['results'][0].to_dict(), mock_results[0])
         self.assertDictEqual(
             self.last_notified['results'][1].to_dict(), mock_results[1])
+
+    @patch('pymongo.MongoClient')
+    def test_no_results(self, mock_client):
+        mock_results = []
+        mock_collection = MagicMock()
+        mock_cursor = mock_collection.find.return_value = MagicMock()
+        mock_cursor.count.return_value = 0
+        # we're going to iterate over this mock generator to get our results
+        mock_cursor.__iter__ = MagicMock(return_value=iter(mock_results))
+        blk = MongoDBFind()
+        # mock this method, returns a collection which we also mock
+        blk._evaluate_collection = MagicMock(return_value=mock_collection)
+        self.configure_block(blk, {'enrich': {'exclude_existing': False}})
+        signals = [Signal({'pi': 3.14})]
+        blk.start()
+        blk.process_signals(signals)
+        blk.stop()
+        self.assertEqual(len(self.last_notified['no_results']), 1)
+        self.assertDictEqual(
+            self.last_notified['no_results'][0].to_dict(), {'pi': 3.14})
